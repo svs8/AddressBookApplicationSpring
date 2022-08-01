095fe02bdcef52ba9a07f9c9445deabed6dad22f
svs8 <surakshithvittalshetty@gmail.com>
2022-08-01 17:04:46 +0530

    Added Validations to few more fields

--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -2,6 +2,8 @@ package com.bridgelabz.addressbookapp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -12,11 +14,18 @@ public class AddressBookDTO {
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Last name is Invalid")
     public String lastName;
+    @Pattern(regexp ="(91-)?[0-9]{10}",message="Phone number is not valid")
     public String phoneNumber;
+
+    @NotEmpty(message="email cannot be empty")
     public String email;
+    @NotEmpty(message="address cannot be empty")
     public String address;
+    @NotEmpty(message="city cannot be empty")
     public String city;
+    @NotEmpty(message="state cannot be empty")
     public String state;
+    @NotNull(message="zip cannot be empty")
     public int zip;
 
     public AddressBookDTO(String firstName, String lastName, String phoneNumber, String email,
