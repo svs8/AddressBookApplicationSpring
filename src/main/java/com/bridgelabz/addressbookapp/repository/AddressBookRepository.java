d10b7c1228521aada78c1b977089ee6d29ed6381
svs8 <surakshithvittalshetty@gmail.com>
2022-08-03 08:38:17 +0530

    Sorting the contacts based on City or State

--- src/main/java/com/bridgelabz/addressbookapp/repository/AddressBookRepository.java
+++ src/main/java/com/bridgelabz/addressbookapp/repository/AddressBookRepository.java
@@ -2,6 +2,14 @@ package com.bridgelabz.addressbookapp.repository;
 
 import com.bridgelabz.addressbookapp.model.AddressBookData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AddressBookRepository extends JpaRepository<AddressBookData, Integer> {
+    @Query(value="select * from address_book where city=:city", nativeQuery=true)
+    List<AddressBookData> findContactByCity(String city);
+    @Query(value="select * from address_book where state=:state", nativeQuery=true)
+    List<AddressBookData> findContactByState(String state);
+
 }
